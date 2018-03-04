276ae54760d9400b6503a5e500ec7565d43fb9c7
mohanenm <ethannmohan@gmail.com>
2018-03-04 14:17:13 -0600

    added russell, trying to make tweets lower case....

--- venv/python-bot.py
+++ venv/python-bot.py
@@ -6,6 +6,7 @@ from markovbot import MarkovBot
 
 bot = MarkovBot()
 bot2 = MarkovBot()
+bot3 = MarkovBot()
 
 ''' 
 clean data for shakespeare
@@ -23,20 +24,22 @@ dirname = os.path.dirname(os.path.abspath(__file__))
 freudText = os.path.join(dirname, 'freudCompleteWorks.txt')
 bot.read(freudText)
 freudTweets =  bot.generate_text(25)
-print(freudTweets)
-
-
 '''
 fTwo = (re.sub('[-,_[@?#*"%;()}0-9]', " ", data))
 fThree = (fTwo.replace("SCENE" "ACT", " "))
 finalSText = fThree'''
 
-dirname = os.path.dirname(os.path.abspath(__file__))
-freudText = os.path.join(dirname, 'shakeComplete.txt')
-bot2.read(finalDataShake)
-shakeTweets = bot2.generate_text(15)
-print(shakeTweets)
+shakeText = os.path.join(dirname, 'shakeComplete.txt')
+bot2.read(shakeText)
+shakeTweets = bot2.generate_text(25)
 
+russellText = os.path.join(dirname, 'russelMath.txt')
+bot3.read(russellText)
+russellTweets = bot3.generate_text(25)
+
+print(freudTweets)
+print(shakeTweets)
+print(russellTweets)
 '''
 Keys/Tokens: 
 '''
@@ -44,12 +47,13 @@ cons_key ='TRXh3CDSFBnPuZziANbqssl1l'
 cons_secret ='sIfuEEp6T8qluDkU3S9PLcINoIIqcp0SUTrnwVfvdWhNRlIS6G'
 access_token ='863265431691436032-PH9ASi1r3tfXJY90i4HuCVVpcLhUJ6D'
 access_token_secret ='POslJ4RgWsgL7BzUV1WY7xZaI9YXGMmSIFPwA2vcZt1Uf'
-
 '''
 Login, tweet-period
 '''
 bot.twitter_login(cons_key, cons_secret, access_token, access_token_secret)
 bot.twitter_tweeting_start(days=0, hours=5, minutes=0, keywords=None, prefix=None, suffix = "#Freud")
 bot2.twitter_login(cons_key, cons_secret, access_token, access_token_secret)
-bot2.twitter_tweeting_start(days=0, hours=5, minutes=0, keywords=None, prefix=None, suffix = "#ShakeSpeare") 
+bot2.twitter_tweeting_start(days=0, hours=5, minutes=0, keywords=None, prefix=None, suffix = "#ShakeSpeare")
+bot3.twitter_login(cons_key, cons_secret, access_token, access_token_secret)
+bot3.twitter_tweeting_start(days=0, hours=5, minutes=0, keywords=None, prefix=None, suffix = "#Russell")
 time.sleep(6400)
