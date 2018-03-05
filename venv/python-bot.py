5c69c4e656a85e20e7af2c7006d5716860add61a
mohanenm <ethannmohan@gmail.com>
2018-03-05 17:12:03 -0600

    upd

--- venv/python-bot.py
+++ venv/python-bot.py
@@ -43,10 +43,7 @@ print(russellTweets)
 '''
 Keys/Tokens: 
 '''
-cons_key ='TRXh3CDSFBnPuZziANbqssl1l'
-cons_secret ='sIfuEEp6T8qluDkU3S9PLcINoIIqcp0SUTrnwVfvdWhNRlIS6G'
-access_token ='863265431691436032-PH9ASi1r3tfXJY90i4HuCVVpcLhUJ6D'
-access_token_secret ='POslJ4RgWsgL7BzUV1WY7xZaI9YXGMmSIFPwA2vcZt1Uf'
+
 '''
 Login, tweet-period
 '''
