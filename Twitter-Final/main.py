d37810ff69629a2abc963d3a1475d8f226f0fa62
mohanenm <ethannmohan@gmail.com>
2018-04-22 23:45:02 -0500

    *twitter key --> *api --> *printing received tweets

--- Twitter-Final/main.py
+++ Twitter-Final/main.py
@@ -1,3 +1,10 @@
+import os
+import time
+import re
+import collections
+import nltk
+from nltk import word_tokenize
+from markovbot import MarkovBot
 import numpy as np
 import twitter, re, datetime, pandas as pd
 
@@ -12,6 +19,21 @@ cons_secret ='sIfuEEp6T8qluDkU3S9PLcINoIIqcp0SUTrnwVfvdWhNRlIS6G'
 access_token ='863265431691436032-PH9ASi1r3tfXJY90i4HuCVVpcLhUJ6D'
 access_token_secret ='POslJ4RgWsgL7BzUV1WY7xZaI9YXGMmSIFPwA2vcZt1Uf'
 
+api = twitter.Api(
+    cons_key         =   twitter_keys['cons_key'],
+    cons_secret      =   twitter_keys['cons_secret'],
+    access_token     =   twitter_keys['access_token'],
+    access_token_secret  =   twitter_keys['access_token_secret'],
+    tweet_mode = 'extended'
+)
+type(api)
+
+nietzsche_tweets = api.GetHashTag(screen_name="#nietzsche", count=200, include_rts=False)
+nietzsche_tweets = [_.AsDict() for _ in nietzsche_tweets]
+for element in x:
+    print(element['id'])
+    print(element['full_text'])
+    print('--')
 
 class GetTweets(object):
 
@@ -19,7 +41,7 @@ class GetTweets(object):
     def__init__(self, api, tweet_limit = 200):
 
 
-    self,api = api
+    self.api = api
     self.result_limit = tweet_limit
 
 
