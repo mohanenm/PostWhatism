5068db2a9c81edc5d5663b3ae9943b3b67ad5da0
mohanenm <ethannmohan@gmail.com>
2018-04-25 00:54:50 -0500

    think I am done

--- Twitter-Final/main_twitter.py
+++ Twitter-Final/main_twitter.py
@@ -15,17 +15,12 @@ import re, datetime, pandas as pd
 
 print(twitter_access.tweets_galore.result_nietz)
 
-
-
 '''
 tweet_text1 = russel_tweets
 clean_text1 = [preprocess_text(x, fix_unicode=True, lowercase=True, no_urls=True, no_emails=True,
                                no_phone_numbers=True, no_currency_symbols=True, no_punct=True, no_accents=True)
                for x in russel_tweets]
 '''
-
-
-
 
 ''' PUT TWEETS INTO A DATAFRAME!'''
 
@@ -75,7 +70,6 @@ summaries = "".join(freud_df['text'])
 ngrams_summaries = vect.build_analyzer()(summaries)
 Counter(ngrams_summaries).most_common(20)
 
-
 # One giant string of all tweets! --> russel
 # summaries = "".join(russel_dataframe['text'])
 # ngrams_summaries = vect.build_analyzer()(summaries)
@@ -100,7 +94,6 @@ clean_text = [preprocess_text(x, fix_unicode=True, lowercase=True, no_urls=True,
                               no_phone_numbers=True, no_currency_symbols=True, no_punct=True, no_accents=True)
               for x in tweet_nietzsche]
 
-
 print(tweet_nietzsche)
 
 y = tweets['handle'].map(lambda x: 1 if x == 'nietzsche' else 0).values
@@ -109,21 +102,79 @@ print(max(pd.Series(y).value_counts(normalize=True)))
 y = tweets['handle'].map(lambda x: 1 if x == 'freud' else 0).values
 print(max(pd.Series(y).value_counts(normalize=True)))
 
-
 tfv = TfidfVectorizer(ngram_range=(1, 2), max_features=2000)
 X = tfv.fit_transform(clean_text).todense()
 print(X.shape)
 
+# types of search, regression, etc
+
 from sklearn.model_selection import GridSearchCV
 
 lr = LogisticRegression()
-params = {'penalty': ['l1', 'l2'], 'C':np.logspace(-5,0,100)}
-#Grid searching to find optimal parameters for Logistic Regression
+params = {'penalty': ['l1', 'l2'], 'C': np.logspace(-5, 0, 100)}
+# Grid searching to find optimal parameters for Logistic Regression
 gs = GridSearchCV(lr, param_grid=params, cv=2, verbose=1)
 gs.fit(X, y)
 
 print(gs.best_params_)
 print(gs.best_score_)
-
-
 # Just spent > 30 minutes trying to git pip install to trust scikit download!
+
+from sklearn.model_selection import cross_val_score
+
+accuracies = cross_val_score(LogisticRegression(), X, y, cv=2)
+
+print(accuracies.mean())
+print(1 - y.mean())
+
+# check our model based on generated tweets....
+
+estimator = LogisticRegression(penalty='l2', C=1.0)
+estimator.fit(X, y)
+
+n_test = [
+    # fill in
+]
+
+f_test = [
+    # fill in
+]
+
+Xtest = tfv.transform(n_test)
+pd.DataFrame(estimator.predict_proba(Xtest), columns=["Proba_nietzsche", "Proba_freud"])
+
+Xtest = tfv.transform(f_test)
+pd.DataFrame(estimator.predict_proba(Xtest), columns=["Proba_nietzsche", "Proba_freud"])
+
+# extracting tweets with highest niet, freud proba ==>
+
+estimator.predict_proba(X)
+Probas_x = pd.DataFrame(estimator.predict_proba(X), columns=["Proba_nietzsche", "Proba_freud"])
+joined_x = pd.merge(tweets, Probas_x, left_index=True, right_index=True)
+print(joined_x)
+
+# actually printing those tweets out
+
+# nietzsche
+
+# --> max
+joined_n = joined_x[joined_x['handle'] == "nietzsche"]
+for el in joined_n[joined_n['Proba_nietzsche'] == max(joined_n['Proba_nietzsche'])]['text']:
+    print(el)
+# --> min
+for el in joined_n[joined_n['Proba_nietzsche']==min(joined_n['Proba_nietzsche'])]['text']:
+    print(el)
+
+#freud
+
+# --> max
+joined_f = joined_x[joined_x['handle'] == "freud"]
+for el in joined_f[joined_n['Proba_freud'] == max(joined_f['Proba_freud'])]['text']:
+    print(el)
+# --> min
+for el in joined_f[joined_f['Proba_Hillary']==min(joined_f['Proba_Hillary'])]['text']:
+    print(el)
+
+
+
+
