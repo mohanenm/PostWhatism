56f1c2db23f5a5b2961bdd0692f860db95e2080b
Ethan Mohan <ethannmohan@gmail.com>
2021-06-07 17:36:40 -0400

    Update api_ver.py

--- duhman/api_ver.py
+++ duhman/api_ver.py
@@ -16,16 +16,15 @@ from nltk import word_tokenize
 from tweepy.parsers import JSONParser
 from tweepy.streaming import StreamListener
 
-'''idea for "tweet miner from mike roman via: git_userid = elaiken3'''
 
 '''
 Keys/Tokens: DO NOT PUSH THESE!
 '''
 
-cons_key = 'TRXh3CDSFBnPuZziANbqssl1l'
-cons_secret = 'sIfuEEp6T8qluDkU3S9PLcINoIIqcp0SUTrnwVfvdWhNRlIS6G'
-access_token = '863265431691436032-PH9ASi1r3tfXJY90i4HuCVVpcLhUJ6D'
-access_token_secret = 'POslJ4RgWsgL7BzUV1WY7xZaI9YXGMmSIFPwA2vcZt1Uf'
+cons_key = ''
+cons_secret = ''
+access_token = ''
+access_token_secret = ''
 
 auth = tweepy.OAuthHandler(cons_key, cons_secret)
 auth.set_access_token(access_token, access_token_secret)
